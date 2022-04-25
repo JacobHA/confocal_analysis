71be7791d6656920712c5136f9b495ccc649362f
Jacob Adamczyk <42879357+JacobHA@users.noreply.github.com>
2022-04-25 15:36:16 -0400

    cleanup

--- 3d_vis.py
+++ 3d_vis.py
@@ -6,113 +6,42 @@ Created on Sat Apr  9 14:06:45 2022
 """
 
 
-import scipy.io as spio
-import vtk
-
-phall = spio.loadmat('phall.mat')['PHALLOIDIN_IMG']
-
 import vtk.util.numpy_support as numpy_support
-
-# def numpyToVTK(data):
-#     data_type = vtk.VTK_FLOAT
-#     shape = data.shape
-
-#     flat_data_array = data.flatten()
-#     vtk_data = numpy_support.numpy_to_vtk(num_array=flat_data_array, deep=True, array_type=data_type)
-    
-#     img = vtk.vtkImageData()
-#     img.GetPointData().SetScalars(vtk_data)
-#     img.SetDimensions(shape[0], shape[1], shape[2])
-#     return img
-
-# from voxelfuse.voxel_model import VoxelModel
-# from voxelfuse.mesh import Mesh
-
-# list(np.array(phall, dtype=bool))
-
-# mayavi.mlab.contour3d()
-
-#%%
-
-"""Create a Volume from a numpy array"""
 import numpy as np
-
-# data_matrix = np.zeros([75, 75, 75], dtype=np.uint8)
-# # all voxels have value zero except:
-# data_matrix[0:35,   0:35,  0:35] = 1
-# data_matrix[35:55, 35:55, 35:55] = 2
-# data_matrix[55:74, 55:74, 55:74] = 3
-
 from vedo import Volume, show
+import k3d
 
-#vol = Volume(data_matrix, c=['white','b','g','r'])
-#vol.addScalarBar3D()
-
-#show(vol, __doc__, axes=1).close()
-
-#%%
-
-"""Create a Volume from a numpy.mgrid"""
-import numpy as np
-from vedo import Volume, Text2D, show
-
-
-import scipy.io as spio
-import vtk
+MIN_PHALL_THRESHOLD = 4
+MIN_DAPI_THRESHOLD = 15
+XY_SCALE = 0.176 # um/pixel
+Z_SCALE = 2.5 # um/pixel
 
 phall = spio.loadmat('phall.mat')['PHALLOIDIN_IMG']
 dapi = spio.loadmat('dapi.mat')['DAPI_IMG']
 
-phall[phall <= 4] = 0
-# phall_max = np.where(phall <= 20)[0].max()
-# phall[phall >= 20] = phall_max
+# Use this to flush out small values below a threshold:
+# (could alternatively use Otsu's method, etc.)
+phall[phall <= MIN_PHALL_THRESHOLD] = 0
+dapi[dapi <= MIN_DAPI_THRESHOLD] = 0
 
-dapi[dapi <= 15] = 0
-# dapi_max = np.where(phall <= 200)[0].max()
-# dapi[dapi >= 200] = dapi_max
-
-
-import vtk.util.numpy_support as numpy_support
-
-
-# Stretch out z axis:
-
+# File in 3D data from matlab:
 X, Y, Z = np.mgrid[:512, :512, :10]
-# Distance from the center at (15, 15, 15)
-# scalar_field = ((X-15)**2 + (Y-15)**2 + (Z-15)**2) /225
-scalar_field1 = phall[X,Y,Z]
-scalar_field2 = dapi[X,Y,Z]
+phall_field = phall[X,Y,Z]
+dapi_field = dapi[X,Y,Z]
 
-vol1 = Volume(scalar_field1, mapper='smart',spacing=(2.5,0.176,0.176))#, mapper='smart')#,mode=1)
-vol2 = Volume(scalar_field2, mapper='smart',spacing=(2.5,0.176,0.176))#, mapper='smart')#,mode=1)
+# Create the volumes
+phall_vol = Volume(phall_field, mapper = 'smart', spacing = (Z_SCALE, XY_SCALE, XY_SCALE))
+dapi_vol = Volume(dapi_field, mapper = 'smart', spacing = (Z_SCALE, XY_SCALE, XY_SCALE))
+phall_vol.color('red')
+dapi_vol.color('blue')
 
-vol1.color('red')
-vol2.color('blue')
-#vol.addScalarBar3D()
-
-
-# lego = vol.legosurface(vmin=1, vmax=2)
-# lego.cmap('hot_r', vmin=1, vmax=2).addScalarBar3D()
-
-# text1 = Text2D(__doc__, c='blue')
-# text2 = Text2D('..and its lego isosurface representation\nvmin=1, vmax=2', c='dr')
-
-# show(vol1, vol2, azimuth=10, interactive=0, offscreen=1)#, interactive=True)
-# show([(vol,text1), (lego,text2)], N=2, azimuth=10).close()
-
-# from vedo import exportWindow   
-
-# exportWindow('panther.html')
-
-# %%
-import k3d
-import numpy as np
-# from vtkplotter import load, datadir
 plot = k3d.plot()
 
+# Use some colormaps
 cmap1 = k3d.colormaps.matplotlib_color_maps.winter
 cmap2 = k3d.colormaps.matplotlib_color_maps.autumn
 
+# Plot both datasets, with labels
 for vol,col,name,alpha in zip([vol1, vol2], [cmap1, cmap2], ['Phalloidin - Cytoskeleton','DAPI - Nuclei'], [30,5]):
     kx, ky, kz = vol.dimensions()
     arr = vol.getPointArray()
@@ -121,5 +50,6 @@ for vol,col,name,alpha in zip([vol1, vol2], [cmap1, cmap2], ['Phalloidin - Cytos
     kvol = k3d.volume(kimage, alpha_coef=alpha, bounds=vol.bounds(), name=name, color_map = col)
     plot += kvol
 
+# Saving the html file for later viewing or sharing
 with open('page.html','w') as fp:
     fp.write(plot.get_snapshot())
