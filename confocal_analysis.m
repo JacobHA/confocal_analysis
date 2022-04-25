1cda135f6d8a15db06cd57e3dd3cbd229945cfce
Jacob Adamczyk <42879357+JacobHA@users.noreply.github.com>
2022-04-25 15:28:29 -0400

    updated matlab

--- confocal_analysis.m
+++ confocal_analysis.m
@@ -5,8 +5,9 @@ Z_SCALE = 2.5; % um/pixel
 
 disp("Relative voxel scale:");
 disp(XY_SCALE / Z_SCALE);
-% Working with a propietary filetype, .lsm 
-% To access it, install it from:
+
+% Working with a propietary filetype: .lsm 
+% To access it, install:
 % https://www.openmicroscopy.org/bioformats/downloads/ 
 % And add it to the MATLAB path (HOME -> Set Path)
 
@@ -33,28 +34,23 @@ end
 % fill holes to calculate area at each slice:
 
 PHALL_VOL_Filled = 0;
-PHALL_Area_list = [];
 PHALLOIDIN_IMG = [];
 for k = 1 : length(PHALLOIDIN)
     phalloidin_slice = imfill(cell2mat(PHALLOIDIN(k)));%, 'holes');
     PHALLOIDIN_IMG(:,:,k) = phalloidin_slice; 
     stats = regionprops(phalloidin_slice, 'Area');
     slice_area = sum([stats.Area])*XY_SCALE^2 ;
-    PHALL_Area_list = [PHALL_Area_list slice_area];
+    % Here we calculate the volume
     PHALL_VOL_Filled = PHALL_VOL_Filled + slice_area * Z_SCALE;
 end
 
 disp('Riemann Sum Approach [um^3]:')
 disp(PHALL_VOL_Filled);
 
-% Now do spherical approximation
-equiv_radius = (max(slice_area)/ pi)^(1/2);
-equiv_vol = (4/3)*pi*equiv_radius^3;
-disp('Equivalent Sphere Approach [um^3]:');
-disp(equiv_vol);
 
+% This will allow you to view the data in MATLAB (alternatively, go to Volume Viewer in APPS)
 % volshow(PHALLOIDIN_IMG, 'ScaleFactors', [XY_SCALE XY_SCALE Z_SCALE]);
-% volshow(DAPI_IMG, 'ScaleFactors', [XY_SCALE XY_SCALE Z_SCALE], 'CameraUpVector', [0 0 1]);
+% volshow(DAPI_IMG, 'ScaleFactors', [XY_SCALE XY_SCALE Z_SCALE]);
 
-% save C:/Users/jacob/OneDrive/Desktop/phall.mat PHALLOIDIN_IMG
-% save C:/Users/jacob/OneDrive/Desktop/dapi.mat DAPI_IMG
+save phall.mat PHALLOIDIN_IMG
+save dapi.mat DAPI_IMG
